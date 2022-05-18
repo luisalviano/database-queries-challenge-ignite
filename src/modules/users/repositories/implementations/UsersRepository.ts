a200ed6fb117f38a1cfd0a9a2c179c2b0cf10486
Maria Luiza Salviano <mluizasalviano@hotmail.com>
2022-05-17 21:01:03 -0300

    adding challenge files

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,29 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const user = await this.repository.findOne(user_id, { relations: ["games"] });
+
+    if (!user) {
+      throw new Error("User does not exist!");
+    }
+
+    return user;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`
+      SELECT * FROM users
+      ORDER BY first_name ASC
+    `);
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`
+      SELECT * FROM users
+      WHERE UPPER(first_name) LIKE UPPER('${first_name}') AND UPPER(last_name) LIKE UPPER('${last_name}')
+    `);
   }
 }
